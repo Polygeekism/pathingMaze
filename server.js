489dc561d673482865a91bd308f0c20c3925b536
polygeekism <mcmahon.kevin.t@gmail.com>
2018-05-02 22:09:59 -0500

    basic server actually running

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyparser = require('body-parser');
 
 var port = 5000;
 
-app.use(express.static('server/public'));
+app.use(express.static('public'));
 
 app.listen(port, function(){
     console.log('app is listening on port:', port);
