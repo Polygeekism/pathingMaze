803a2d1ce31b23850f580a46254bbe4c64bdce39
polygeekism <mcmahon.kevin.t@gmail.com>
2018-05-02 22:54:24 -0500

    controller added

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,8 @@
 console.log("sourced the javascript");
+
+
+var app = angular.module('myApp', []);
+
+app.controller('mazeController', ['$http', function($http){
+    console.log("controller has been loaded");
+}]);
