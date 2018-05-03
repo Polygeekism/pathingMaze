1ecc75382bab267b51aa0d711220a35b609617f4
Kevin McMahon <kevin.mcmahon@rhris.com>
2018-05-03 13:08:36 -0500

    string is split into pieces by blank spaces.

--- server/routes/maze.js
+++ server/routes/maze.js
@@ -3,8 +3,17 @@ var router = express.Router();
 
 
 router.post('/', function(req, res){
-    console.log('reached the route from front end.', req.body);
+    console.log('reached the route from front end.', req.body.maze);
+    buildMaze(req.body.maze);
     res.sendStatus(200);
 })
+
+function buildMaze(inputArray){
+    var i = 0;
+    var arr = inputArray.split(/\s+/);
+    console.log('buildmaze function', arr);
+    
+
+}
 
 module.exports = router;
