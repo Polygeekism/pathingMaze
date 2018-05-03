18bd12a365e3e0dfc237decb1420503fd770c7f1
polygeekism <mcmahon.kevin.t@gmail.com>
2018-05-02 23:06:12 -0500

    start of data movement

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,4 +5,10 @@ var app = angular.module('myApp', []);
 
 app.controller('mazeController', ['$http', function($http){
     console.log("controller has been loaded");
+    var self = this;
+    self.mazeInput = [];
+
+    self.sendMaze = function(){
+        console.log("button clicked,", self.mazeInput);
+    }
 }]);
