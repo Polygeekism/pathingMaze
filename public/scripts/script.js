bbb7a12cac9f46ca185ae7fc33a947a71a73a6d2
Kevin McMahon <kevin.mcmahon@rhris.com>
2018-05-07 12:42:53 -0500

    graphs are populating correctly.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -23,29 +23,40 @@ app.controller("mazeController", [
       var canvas = document.getElementById("canvas");
 
       var ctx = canvas.getContext("2d");
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      
       var board = maze;
       
 
-      var width = 300;
+      var width = 200;
       var blockSize = width / board.length;
 
       ctx.setTransform(1, 0, 0, 1, 0, 0);
-      ctx.clearRect(0, 0, width, width);
-      ctx.fillStyle = "black";
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      
+      
+      ctx.fillStyle="black";
       //Loop through the board array drawing the walls and the goal
       for (var y = 0; y < board.length; y++) {
         for (var x = 0; x < board[y].length; x++) {
           //Draw a wall
           if (board[y][x] === '#') {
-            ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+            ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);            
             
           }
           //Draw the goal
           else if (board[y][x] === "B") {
             ctx.beginPath();
             ctx.lineWidth = 5;
-            ctx.strokeStyle = "gold";
+            ctx.strokeStyle = "red";
+            ctx.moveTo(x * blockSize, y * blockSize);
+            ctx.lineTo((x + 1) * blockSize, (y + 1) * blockSize);
+            ctx.moveTo(x * blockSize, (y + 1) * blockSize);
+            ctx.lineTo((x + 1) * blockSize, y * blockSize);
+            ctx.stroke();
+          }else if (board[y][x] === "A") {
+            ctx.beginPath();
+            ctx.lineWidth = 5;
+            ctx.strokeStyle = "green";
             ctx.moveTo(x * blockSize, y * blockSize);
             ctx.lineTo((x + 1) * blockSize, (y + 1) * blockSize);
             ctx.moveTo(x * blockSize, (y + 1) * blockSize);
@@ -54,13 +65,14 @@ app.controller("mazeController", [
           }
         }
       }
-      for (var y = 0; y < board.length; y++) {
-        for (var x = 0; x < board[y].length; x++) {
+      ctx.fillStyle = "orange";
+      for (var y = 0; y < board.length ; y++) {
+        for (var x = 0; x < board[y].length ; x++) {
             //console.log('x,y',x,y)
             if(board[y][x] === '@'){
-                //console.log('coord values,',y,x, board[y][x]);
+                console.log('coord values,',y,x, board[y][x]);
                 ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
-                ctx.fillStyle = "orange";
+                
             }
             }
         }
